e11a1f28ba4f64d0b581fa7b65c62cc821bc4bf2
Juan Carlos <juanksoliveira@gmail.com>
2019-05-24 02:25:22 -0300

    DroneControllerPlugin with ref to model and world.

--- src/DroneControllerPlugin.cc
+++ src/DroneControllerPlugin.cc
@@ -3,6 +3,7 @@
 #include <gazebo/physics/physics.hh>
 #include <gazebo/common/common.hh>
 #include <iostream>
+
 namespace gazebo
 {
 class DroneControllerPlugin : public ModelPlugin
@@ -17,6 +18,11 @@ public:
   /////////////////////////////////////////////////////////////////////////////
   void Load(physics::ModelPtr _parent, sdf::ElementPtr /*_sdf*/)
   {
+    std::cout << "DroneControllerPlugin is Loading..." << std::endl;
+    // Store the pointer to the model and world
+    this->model = _parent;
+    this->world = this->model->GetWorld();
+    std::cout << "DroneControllerPlugin has loaded." << std::endl;
   }
 
   /////////////////////////////////////////////////////////////////////////////
@@ -25,6 +31,13 @@ public:
   {
   }
 
+private:
+
+  /// \brief Pointer to the model.
+  physics::ModelPtr model;
+
+  /// \brief World from the model.
+  physics::WorldPtr world;
 };
 
 // Register this plugin with the simulator
