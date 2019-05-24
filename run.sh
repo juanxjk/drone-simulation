45ae7a4f8f6b50e5ad2010ac245e3708c7eac8d1
Juan Carlos <juanksoliveira@gmail.com>
2019-05-24 02:36:58 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
-mkdir build
-cd build
-cmake ../
-make
-export GAZEBO_PLUGIN_PATH=`pwd`:$GAZEBO_PLUGIN_PATH
-cd ..
-cp -r ./models/quad_2292_1/ ~/.gazebo/models/
-gazebo -u --verbose ./drone_simulation.world
+#!/bin/sh
+# Plugin Compile
+if [ ! -d "./build" ]; then mkdir build; fi
+ cd build && 
+ cmake ../ && 
+ make &&
+
+# Setup
+export GAZEBO_PLUGIN_PATH=`pwd`:$GAZEBO_PLUGIN_PATH &&
+echo  'GAZEBO_PLUGIN_PATH =' $GAZEBO_PLUGIN_PATH
+cd .. &&
+export GAZEBO_MODEL_PATH=`pwd`/models/:$GAZEBO_MODEL_PATH
+echo 'GAZEBO_MODEL_PATH = ' $GAZEBO_MODEL_PATH
+
+# Run
+ gazebo -u --verbose ./drone_simulation.world
